3caf73c86844a07c5680df10a4e034fd45224ae1
弗洛林 <75491401+fazy1729@users.noreply.github.com>
2025-04-12 18:22:16 +0300

    index.ejs

--- index.js
+++ index.js
@@ -1,152 +1,40 @@
-const express= require("express");
-const path= require("path");
-const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
 
-
-app= express();
-
-v=[10,27,23,44,15]
-
-nrImpar=v.find(function(elem){return elem % 100 == 1})
-console.log(nrImpar)
-
-console.log("Folderul proiectului: ", __dirname)
-console.log("Calea fisierului index.js: ", __filename)
-console.log("Folderul curent de lucru: ", process.cwd())
-
+// Configurare
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-obGlobal={
-    obErori:null
-}
+// Resurse statice
+app.use("/resurse", express.static(path.join(__dirname, "resurse")));
 
+// Rute
+app.get("/", (req, res) => {
+    res.render("index", { 
+        ip: req.ip,
+        titlu: "SciMind - Pagina principală" 
+    });
+});
 
-function initErori(){
-    let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
-    console.log(continut)
-    obGlobal.obErori=JSON.parse(continut)
-    console.log(obGlobal.obErori)
-    
-    obGlobal.obErori.eroare_default.imagine=path.join(obGlobal.obErori.cale_baza, obGlobal.obErori.eroare_default.imagine)
-    for (let eroare of obGlobal.obErori.info_erori){
-        eroare.imagine=path.join(obGlobal.obErori.cale_baza, eroare.imagine)
-    }
-    console.log(obGlobal.obErori)
+app.get("/favicon.ico", (req, res) => {
+    res.sendFile(path.join(__dirname, "resurse/ico/favicon.ico"));
+});
 
-}
+// 404 - Trebuie să fie ultima rută!
+app.use((req, res) => {
+    res.status(404).render("pagini/eroare", {
+        titlu: "Pagina nu există",
+        text: "Rețeaua neuronală nu a putut găsi resursa cerută.",
+        imagine: "/resurse/imagini/eroare/404.jpg"
+    });
+});
 
-initErori()
-
-function afisareEroare(res, identificator, titlu, text, imagine){
-    let eroare= obGlobal.obErori.info_erori.find(function(elem){ 
-                        return elem.identificator==identificator
-                    });
-    if(eroare){
-        if(eroare.status)
-            res.status(identificator)
-        var titluCustom=titlu || eroare.titlu;
-        var textCustom=text || eroare.text;
-        var imagineCustom=imagine || eroare.imagine;
-
-
-    }
-    else{
-        var err=obGlobal.obErori.eroare_default
-        var titluCustom=titlu || err.titlu;
-        var textCustom=text || err.text;
-        var imagineCustom=imagine || err.imagine;
-
-
-    }
-    res.render("pagini/eroare", { //transmit obiectul locals
-        titlu: titluCustom,
-        text: textCustom,
-        imagine: imagineCustom
-})
-
-}
-
-
-
-app.use("/resurse", express.static(path.join(__dirname,"resurse")))
-app.get("/favicon.ico", function(req, res){
-    res.sendFile(path.join(__dirname, "resurse/imagini/favicon/favicon.ico"))
-})
-
-app.get(["/","/index","/home"], function(req, res){
-    res.render("pagini/index",{ip:req.ip});
-})
-
-// app.get("/despre", function(req, res){
-//     res.render("pagini/despre");
-// })
-
-app.get("/index/a", function(req, res){
-    res.render("pagini/index");
-})
-
-
-app.get("/cerere", function(req, res){
-    res.send("<p style='color:blue'>Buna ziua</p>")
-})
-
-
-app.get("/fisier", function(req, res, next){
-    res.sendfile(path.join(__dirname,"package.json"));
-})
-
-
-app.get("/abc", function(req, res, next){
-    res.write("Data curenta: ")
-    next()
-})
-
-app.get("/abc", function(req, res, next){
-    res.write((new Date())+"")
-    res.end()
-    next()
-})
-
-
-app.get("/abc", function(req, res, next){
-    console.log("------------")
-})
-
-app.get("/*.ejs", function(req, res, next){
-    afisareEroare(res,400);
-})
-
-
-app.get("/*", function(req, res, next){
-    try{
-        res.render("pagini"+req.url,function (err, rezultatRandare){
-            if (err){
-                if(err.message.startsWith("Failed to lookup view")){
-                    afisareEroare(res,404);
-                }
-                else{
-                    afisareEroare(res);
-                }
-            }
-            else{
-                console.log(rezultatRandare);
-                res.send(rezultatRandare)
-            }
-        });
-    }
-    catch(errRandare){
-        if(errRandare.message.startsWith("Cannot find module")){
-            afisareEroare(res,404);
-        }
-        else{
-            afisareEroare(res);
-        }
-    }
-})
-
-
-
-app.listen(8080);
-console.log("Serverul a pornit")
-
-
+// Pornire
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`
+    Serverul rulează pe portul ${PORT}
+    Accesează: http://localhost:${PORT}
+    `);
+});
